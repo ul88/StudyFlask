ad030794e00286e86510f00434a421f6a7cd6f32
ul88 <hkimog13@naver.com>
2024-07-31 17:26:36 +0900

    2장 마무리

--- JumpToFlask2/pybo/views/question_views.py
+++ JumpToFlask2/pybo/views/question_views.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for
+from werkzeug.utils import redirect
+from datetime import datetime
 
+from .. import db
 from pybo.models import Question
+from pybo.forms import QuestionForm, AnswerForm
 
 bp = Blueprint('question',__name__, url_prefix='/question')
 
@@ -11,5 +15,16 @@ def _list():
 
 @bp.route('detail/<int:question_id>')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get(question_id)
-    return render_template('question/question_detail.html',question = question)
+    return render_template('question/question_detail.html',question = question, form = form)
+
+@bp.route('/create', methods=["POST","GET"])
+def create():
+    form = QuestionForm()
+    if request.method == "POST" and form.validate_on_submit():
+        question = Question(subject = form.subject.data, content = form.content.data, create_date = datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    return render_template('question/question_form.html', form = form)
